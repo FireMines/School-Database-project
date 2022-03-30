705b6b44adfde49bc1f046e85efde858afd9d0ef
Matthias Greeven <matthidg@stud.ntnu.no>
2022-03-30 19:04:13 +0200

    fixed some connections, added prodPlanRef and auth

--- projectdb.sql
+++ projectdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 28, 2022 at 04:54 PM
+-- Generation Time: Mar 30, 2022 at 07:03 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.14
 
@@ -20,6 +20,17 @@ SET time_zone = "+00:00";
 --
 -- Database: `projectdb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authenticator`
+--
+
+CREATE TABLE `authenticator` (
+  `token` varchar(50) COLLATE utf8_danish_ci NOT NULL,
+  `role` varchar(50) COLLATE utf8_danish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 -- --------------------------------------------------------
 
@@ -68,16 +79,10 @@ INSERT INTO `employee` (`employeeNumber`, `name`, `department`) VALUES
 CREATE TABLE `franchise` (
   `customerID` int(20) NOT NULL,
   `name` varchar(50) COLLATE utf8_danish_ci NOT NULL,
-  `buying_price` float NOT NULL,
-  `shipping_address` varchar(30) COLLATE utf8_danish_ci NOT NULL
+  `buying_price` decimal(15,2) NOT NULL,
+  `shipping_address` varchar(30) COLLATE utf8_danish_ci NOT NULL,
+  `Franchise` varchar(50) COLLATE utf8_danish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
-
---
--- Dumping data for table `franchise`
---
-
-INSERT INTO `franchise` (`customerID`, `name`, `buying_price`, `shipping_address`) VALUES
-(2, 'BigmanINC', 9000, 'Habbo Club Veien 17');
 
 -- --------------------------------------------------------
 
@@ -105,19 +110,13 @@ INSERT INTO `franchise_store` (`name`, `shipping`) VALUES
 
 CREATE TABLE `orders` (
   `orderNumber` int(20) NOT NULL,
+  `customer_id` int(20) NOT NULL,
   `quantity` int(2) NOT NULL,
-  `totalPrice` float NOT NULL,
+  `totalPrice` decimal(15,2) NOT NULL,
   `state` enum('new','open','available','cancelled','ready','shipped') COLLATE utf8_danish_ci NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `productID` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`orderNumber`, `quantity`, `totalPrice`, `state`, `date`) VALUES
-(100100, 10, 1000, 'available', '2022-03-17 18:50:38'),
-(6969669, 5, 905, 'new', '2022-03-30 13:50:17');
 
 -- --------------------------------------------------------
 
@@ -127,9 +126,21 @@ INSERT INTO `orders` (`orderNumber`, `quantity`, `totalPrice`, `state`, `date`) 
 
 CREATE TABLE `productionplan` (
   `employeeNumber` int(20) NOT NULL,
-  `typeID` int(11) NOT NULL,
+  `planID` int(11) NOT NULL,
   `startDate` date NOT NULL,
   `endDate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productionplanreference`
+--
+
+CREATE TABLE `productionplanreference` (
+  `planID` int(11) NOT NULL,
+  `productID` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 -- --------------------------------------------------------
@@ -142,18 +153,10 @@ CREATE TABLE `shipment` (
   `shipmentNumber` int(20) NOT NULL,
   `orderNumber` int(20) NOT NULL,
   `transporterID` int(20) NOT NULL,
-  `customerID` int(20) NOT NULL,
   `shippingAddress` varchar(100) COLLATE utf8_danish_ci NOT NULL,
   `pickUpDate` timestamp NOT NULL DEFAULT current_timestamp(),
   `state` enum('ready','shipped') COLLATE utf8_danish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
-
---
--- Dumping data for table `shipment`
---
-
-INSERT INTO `shipment` (`shipmentNumber`, `orderNumber`, `transporterID`, `customerID`, `shippingAddress`, `pickUpDate`, `state`) VALUES
-(1, 100100, 1, 1, 'FC habbo', '2022-03-26 15:01:31', 'shipped');
 
 -- --------------------------------------------------------
 
@@ -182,7 +185,7 @@ CREATE TABLE `skitype` (
   `description` varchar(200) COLLATE utf8_danish_ci NOT NULL,
   `historical` tinyint(1) DEFAULT NULL,
   `url` varchar(255) COLLATE utf8_danish_ci NOT NULL,
-  `msrp` float DEFAULT NULL
+  `msrp` decimal(15,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 -- --------------------------------------------------------
@@ -194,7 +197,7 @@ CREATE TABLE `skitype` (
 CREATE TABLE `store` (
   `name` varchar(50) COLLATE utf8_danish_ci NOT NULL,
   `customerID` int(20) NOT NULL,
-  `price` float NOT NULL,
+  `price` decimal(15,2) NOT NULL,
   `address` varchar(30) COLLATE utf8_danish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
@@ -258,7 +261,8 @@ ALTER TABLE `employee`
 -- Indexes for table `franchise`
 --
 ALTER TABLE `franchise`
-  ADD PRIMARY KEY (`customerID`);
+  ADD PRIMARY KEY (`customerID`),
+  ADD KEY `Franchise_ibfk_2` (`Franchise`);
 
 --
 -- Indexes for table `franchise_store`
@@ -270,15 +274,23 @@ ALTER TABLE `franchise_store`
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`orderNumber`);
+  ADD PRIMARY KEY (`orderNumber`),
+  ADD KEY `Orders_Customer_FK` (`customer_id`),
+  ADD KEY `Orders_Ski_FK` (`productID`);
 
 --
 -- Indexes for table `productionplan`
 --
 ALTER TABLE `productionplan`
-  ADD PRIMARY KEY (`startDate`),
-  ADD KEY `employeeNumber` (`employeeNumber`),
-  ADD KEY `typeID` (`typeID`);
+  ADD PRIMARY KEY (`planID`),
+  ADD KEY `employeeNumber` (`employeeNumber`);
+
+--
+-- Indexes for table `productionplanreference`
+--
+ALTER TABLE `productionplanreference`
+  ADD PRIMARY KEY (`planID`),
+  ADD KEY `PlanReference_Ski_FK` (`productID`);
 
 --
 -- Indexes for table `shipment`
@@ -286,7 +298,6 @@ ALTER TABLE `productionplan`
 ALTER TABLE `shipment`
   ADD PRIMARY KEY (`shipmentNumber`),
   ADD KEY `transporterID` (`transporterID`),
-  ADD KEY `customerID` (`customerID`),
   ADD KEY `orderNumber` (`orderNumber`);
 
 --
@@ -387,21 +398,34 @@ ALTER TABLE `transporter`
 -- Constraints for table `franchise`
 --
 ALTER TABLE `franchise`
+  ADD CONSTRAINT `Franchise_ibfk_2` FOREIGN KEY (`Franchise`) REFERENCES `franchise_store` (`name`),
   ADD CONSTRAINT `franchise_ibfk_1` FOREIGN KEY (`customerID`) REFERENCES `customer` (`customerID`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `Orders_Customer_FK` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customerID`),
+  ADD CONSTRAINT `Orders_Ski_FK` FOREIGN KEY (`productID`) REFERENCES `ski` (`productID`);
 
 --
 -- Constraints for table `productionplan`
 --
 ALTER TABLE `productionplan`
-  ADD CONSTRAINT `productionplan_ibfk_1` FOREIGN KEY (`employeeNumber`) REFERENCES `employee` (`employeeNumber`),
-  ADD CONSTRAINT `productionplan_ibfk_2` FOREIGN KEY (`typeID`) REFERENCES `skitype` (`typeID`);
+  ADD CONSTRAINT `productionplan_ibfk_1` FOREIGN KEY (`employeeNumber`) REFERENCES `employee` (`employeeNumber`);
+
+--
+-- Constraints for table `productionplanreference`
+--
+ALTER TABLE `productionplanreference`
+  ADD CONSTRAINT `PlanReference_Plan_FK` FOREIGN KEY (`planID`) REFERENCES `productionplan` (`planID`),
+  ADD CONSTRAINT `PlanReference_Ski_FK` FOREIGN KEY (`productID`) REFERENCES `ski` (`productID`);
 
 --
 -- Constraints for table `shipment`
 --
 ALTER TABLE `shipment`
   ADD CONSTRAINT `shipment_ibfk_1` FOREIGN KEY (`transporterID`) REFERENCES `transporter` (`transporterID`),
-  ADD CONSTRAINT `shipment_ibfk_2` FOREIGN KEY (`customerID`) REFERENCES `customer` (`customerID`),
   ADD CONSTRAINT `shipment_ibfk_3` FOREIGN KEY (`orderNumber`) REFERENCES `orders` (`orderNumber`);
 
 --
