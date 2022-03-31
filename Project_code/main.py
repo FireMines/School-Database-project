d8d115c0e11e5b71cc24fec2b1b7787c7e85f0e1
lilliaaw <lilliaaw@stud.ntnu.no>
2022-03-31 14:10:23 +0200

    Implemented POST for product planner

--- Project_code/main.py
+++ Project_code/main.py
@@ -527,6 +527,45 @@ def production_planner():
         cur.close()
         return jsonify(plan_info),201
 
+    #           #
+    #   POST    #
+    #           #
+    elif request.method == 'POST':
+        data = request.get_json()
+        plan_id = data['planID']
+        employee_id = data['employeeID']
+        start_date=data['startDate']
+        end_date=data['endDate']
+        referneces=data['planReferences']
+
+        cur=mysql.connection.cursor()
+
+        plan_info= cur.execute("SELECT * FROM `productionplan` WHERE `planID`=%s", (plan_id,))
+
+        if plan_info <=0:
+            
+            change_state = cur.execute("INSERT INTO `productionplan` (`employeeNumber`, `planID`, `startDate`, `endDate`) VALUES (%s,%s,%s,%s)", (employee_id, plan_id, start_date, end_date,))
+            mysql.connection.commit()
+
+            for ref in referneces:
+                ref_id=ref['referenceID']
+                product_id=ref['productID']
+                quantity=ref['quantity']
+
+                change_state== cur.execute("INSERT INTO `productionplanreference` (`planID`, `productID`, `ReferenceID`, `Quantity`) VALUES (%s,%s,%s,%s)", (plan_id, product_id, ref_id, quantity,))
+                mysql.connection.commit()
+
+            plan_info= cur.execute("SELECT * FROM `productionplanreference` INNER JOIN `productionplan` ON `productionplanreference`.`planID`=`productionplan`.`planID` WHERE `productionplan`.`planID`=%s", (plan_id,))
+
+            if plan_info >0:
+                plan_info = cur.fetchall()
+
+            cur.close()
+            return jsonify(plan_info),201
+        else :
+            cur.close()
+            return "This plan already exist in the database",400
+
     else:
         print("Method not implemented! Choose between GET or POST instead")
 
