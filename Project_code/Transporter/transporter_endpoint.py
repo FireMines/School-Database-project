7be65aa3671a2e24df3db31c475deaa0116a01ce
Matthias Greeven <matthidg@stud.ntnu.no>
2022-05-15 17:15:41 +0200

    added small comments

--- Project_code/Transporter/transporter_endpoint.py
+++ Project_code/Transporter/transporter_endpoint.py
@@ -8,11 +8,15 @@ import http
 #                                   #
 def get_transporter_info():
     
+    #           #
+    #   GET     #
+    #           #
+    
     if request.method == 'GET':
         
         cur=consts.mysql.connection.cursor()
 
-        transporter_info= cur.execute("SELECT * FROM `shipment`")
+        transporter_info= cur.execute("SELECT * FROM `shipment`") # Get all the shipments
 
         if transporter_info >0:
             transporter_info = cur.fetchall()
@@ -20,8 +24,14 @@ def get_transporter_info():
         cur.close()
         return jsonify(transporter_info),http.HTTPStatus.OK
     
+    
+    #           #
+    #   PUT     #
+    #           #
     elif request.method == 'PUT':
         data = request.get_json()
+        
+        # We need these things from the user
         shipmentNumber=data['shipmentNumber']
         transport_id=data['transporterID']
         status=data['state']
@@ -31,7 +41,8 @@ def get_transporter_info():
         change_order_state = cur.execute("UPDATE `shipment` SET `state`=%s WHERE `shipmentNumber`=%s AND `transporterID`=%s",(status,shipmentNumber,transport_id,))
         consts.mysql.connection.commit()
 
-        account_info= cur.execute("SELECT * FROM `shipment`")
+        # Show all relevant shipments
+        account_info= cur.execute("SELECT * FROM `shipment` WHERE `shipmentNumber`=%s AND `transporterID`=%s",(shipmentNumber,transport_id,))
 
         if account_info >0:
             account_info = cur.fetchall()
