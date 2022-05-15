1991adc0c7d1eb566f50fc67f43726343a211910
Matthias Greeven <matthidg@stud.ntnu.no>
2022-05-15 17:27:34 +0200

    fixed small grammar mistakes

--- Project_code/ProductionPlanner/productionplanner_endpoint.py
+++ Project_code/ProductionPlanner/productionplanner_endpoint.py
@@ -17,10 +17,10 @@ def production_planner():
         
         cur=consts.mysql.connection.cursor()
 
-        if data:
+        if data: #this is the filter, it only retrieves the desired plan
             plan_id =data['planID']
             plan_info= cur.execute("SELECT * FROM `productionplanreference` INNER JOIN `productionplan` ON `productionplanreference`.`planID`=`productionplan`.`planID` WHERE `productionplan`.`planID`=%s", (plan_id,))
-        else:
+        else: #Retrieve all plans
             plan_info= cur.execute("SELECT * FROM `productionplan`")
 
         if plan_info >0:
@@ -38,18 +38,20 @@ def production_planner():
         employee_id = data['employeeID']
         start_date=data['startDate']
         end_date=data['endDate']
-        referneces=data['planReferences']
+        references=data['planReferences']
 
         cur=consts.mysql.connection.cursor()
 
         plan_info= cur.execute("SELECT * FROM `productionplan` WHERE `planID`=%s", (plan_id,))
 
-        if plan_info <=0:
+        if plan_info <=0: #Does this plan already exist?
             
+            #Create a new plan
             change_state = cur.execute("INSERT INTO `productionplan` (`employeeNumber`, `planID`, `startDate`, `endDate`) VALUES (%s,%s,%s,%s)", (employee_id, plan_id, start_date, end_date,))
             consts.mysql.connection.commit()
 
-            for ref in referneces:
+            #Create new references
+            for ref in references:
                 ref_id=ref['referenceID']
                 product_id=ref['productID']
                 quantity=ref['quantity']
@@ -57,6 +59,7 @@ def production_planner():
                 change_state== cur.execute("INSERT INTO `productionplanreference` (`planID`, `productID`, `ReferenceID`, `Quantity`) VALUES (%s,%s,%s,%s)", (plan_id, product_id, ref_id, quantity,))
                 consts.mysql.connection.commit()
 
+            #Fetch the relevant plans that we print out to user
             plan_info= cur.execute("SELECT * FROM `productionplanreference` INNER JOIN `productionplan` ON `productionplanreference`.`planID`=`productionplan`.`planID` WHERE `productionplan`.`planID`=%s", (plan_id,))
 
             if plan_info >0:
